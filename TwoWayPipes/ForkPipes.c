0aa50e58bb470ea63f0478ac9bf1253e7eecc23c
Egzon Arifi <egzon.arifi@jaywalker-digital.ch>
2017-05-03 20:02:45 +0200

    Changes code format

--- TwoWayPipes/ForkPipes.c
+++ TwoWayPipes/ForkPipes.c
@@ -18,23 +18,19 @@ int main(int argc, char *argv[])
     int len, i;
     pid_t pid;
     /* create all the descriptor pairs we need */
-    for (i=0; i<NUM_PIPES; ++i)
-    {
-        if (pipe(fd+(i*2)) < 0)
-        {
+    for (i=0; i<NUM_PIPES; ++i) {
+        if (pipe(fd+(i*2)) < 0) {
             printf("Failed to allocate pipes");
             exit(EXIT_FAILURE);
         }
     }
     /* fork() returns 0 for child process, child-pid for parent process. */
-    if ((pid = fork()) < 0)
-    {
+    if ((pid = fork()) < 0) {
         perror("Failed to fork process");
         return EXIT_FAILURE;
     }
     /* if the pid is zero, this is the child process */
-    if (pid == 0)
-    {
+    if (pid == 0) {
         // Child. Start by closing descriptors we
         //  don't need in this process
         // Parent. close unneeded descriptors
@@ -45,24 +41,21 @@ int main(int argc, char *argv[])
         printf("Child with pid %d: Sending to parent\n", pid);
         /* send a value to parent */
         int data [5] = {6,7,8,9,10};
-        if(  write(fd[P2_WRITE], &data, sizeof(int)*5) == -1)
-        {
+        if(  write(fd[P2_WRITE], &data, sizeof(int)*5) == -1) {
             perror("Child: Failed to send value to parent ");
             exit(EXIT_FAILURE);
         }
         /* now wait for a response */
         int buf [5] = {};
         len = read(fd[P2_READ], &buf, sizeof(int)*5);
-        for (int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             printf("\nElements recieved from child is %d\n ",buf[i]);
         }
-        if (len < 0)
-        {
+        if (len < 0) {
             perror("Child: failed to read value from pipe");
             exit(EXIT_FAILURE);
         }
-        else if (len == 0)
-        {
+        else if (len == 0) {
             /* not an error, but certainly unexpected */
             fprintf(stderr, "Child(%d): Read EOF from pipe", pid);
         }
@@ -71,9 +64,7 @@ int main(int argc, char *argv[])
         close(fd[P2_WRITE]);
         
         return EXIT_SUCCESS;
-    }
-    else
-    {
+    } else {
         /* Parent. close unneeded descriptors */
         close(fd[P2_READ]);
         close(fd[P2_WRITE]);
@@ -84,8 +75,7 @@ int main(int argc, char *argv[])
         printf("Parent with pid %d: Sending to child\n", pid);
         int data [5] = {1,2,3,4,5};
         sleep(2);
-        if(  write(fd[P1_WRITE], &data, sizeof(int)*5) == -1)
-        {
+        if(  write(fd[P1_WRITE], &data, sizeof(int)*5) == -1) {
             perror("Parent: Failed to send value to child ");
             exit(EXIT_FAILURE);
         }
@@ -93,17 +83,15 @@ int main(int argc, char *argv[])
         /* now wait for a response */
         int buf [5] = {};
         len = read(fd[P1_READ], &buf, sizeof(int)*5);
-        for (int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             printf("\nElements recieved from parent is %d\n ",buf[i]);
         }
         kill(childPid, SIGKILL);
-        if (len < 0)
-        {
+        if (len < 0) {
             perror("Parent: failed to read value from pipe");
             exit(EXIT_FAILURE);
         }
-        else if (len == 0)
-        {
+        else if (len == 0) {
             /* not an error, but certainly unexpected */
             fprintf(stderr, "Parent(%d): Read EOF from pipe", pid);
         }
