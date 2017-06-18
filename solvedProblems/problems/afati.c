5759e28145fc058e3c171bcd4443a1b9a4afeb97
Egzon Arifi <egzon.arifi@jaywalker-digital.ch>
2017-06-18 15:07:56 +0200

    new folder with solved problems

---
+++ solvedProblems/problems/afati.c
@@ -0,0 +1,28 @@
+/*
+ Solved problems
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <getopt.h>
+#include <stdbool.h>
+
+//Detyra 1.1
+
+//Detyra 1.2a
+
+//Detyra 1.2b
+
+//Detyra 2
+
+int main(int argc, char * argv[]) {
+
+    //Detyra 1.1
+    
+    //Detyra 1.2b
+   
+    //Detyra 2
+    
+    return 0;
+}
