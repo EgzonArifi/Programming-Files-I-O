46271cd52f1ac2b6e9e50d8869fdc04bbcfe492a
Egzon Arifi <egzon.arifi@jaywalker-digital.ch>
2017-06-18 16:02:44 +0200

    added new solved problem 1.2

--- solvedProblems/problems/afati.c
+++ solvedProblems/problems/afati.c
@@ -15,8 +15,14 @@ int isDivisor(int d, int n) {
     }
     return 0;
 }
-//Detyra 1.2a
-
+//Detyra 1.2
+int isDivisorByReference(int d, int n, int *fR) {
+    *fR = n;
+    if ((d % n) == 0) {
+        return 1;
+    }
+    return 0;
+}
 //Detyra 1.2b
 
 //Detyra 2
@@ -27,9 +33,14 @@ int main(int argc, char * argv[]) {
     int d, n;
     printf("Jenpni numrat: ");
     scanf("%i %i",&d, &n);
-    printf("jane te plotepjestueshem: %d\n", isDivisor(d, n));
-    //Detyra 1.2b
-   
+    printf("a jane te plotepjestueshem?: %d\n", isDivisor(d, n));
+    
+    //Detyra 1.2
+    int dR, nR, fR;
+    printf("Jenpni numrat: ");
+    scanf("%i %i", &dR, &nR);
+    printf("a jane te plotepjestueshem?: %d\n", isDivisorByReference(dR, nR, &fR));
+    printf("Faktori i pjestueshmerise: %d\n", fR);
     //Detyra 2
     
     return 0;
