64d8e3f3c44994852ecd2bdff25e77d61874e15e
Egzon Arifi <egzon.arifi@jaywalker-digital.ch>
2017-06-27 22:02:49 +0200

    added example to read characters from the file

--- Exercises/i:o/exampleio.c
+++ Exercises/i:o/exampleio.c
@@ -7,6 +7,7 @@
 #define kFileName "file_to_write"
 int main(int argc, char * argv[]) {
     FILE *dataFile;
+    char ch;
     
     /* Writing something */
     if ((dataFile = fopen(kFileName,"a")) == NULL) {
@@ -15,8 +16,22 @@ int main(int argc, char * argv[]) {
     }
     char sentence[] = "This is a test\n";
     fwrite(sentence, strlen(sentence), 1, dataFile);
+    fclose(dataFile);
     
     printf("Success\n");
     
+    /* Readinf from the file */
+    if ((dataFile = fopen(kFileName,"r")) == NULL) {
+        printf("Error while reading from file\n");
+        exit(0);
+    }
+    
+    /* character read */
+    printf("Reading for each character\n");
+    while ((ch = fgetc(dataFile)) != EOF) {
+        printf("%c",ch);
+    }
+    printf("\n");
+    fclose(dataFile);
     return 0;
 }
