a3c68d2b05b86959f0e22d1e603265611e72eca0
Egzon Arifi <egzon.arifi@jaywalker-digital.ch>
2017-07-01 11:11:04 +0200

    created program and directory for signals example

---
+++ signalsExample/signal1.c
@@ -0,0 +1,13 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+int main()
+{    
+    return 0;
+}
