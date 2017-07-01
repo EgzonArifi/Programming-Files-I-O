a758de1518905150ad560b34152ee6033ae4d8ac
Egzon Arifi <egzon.arifi@jaywalker-digital.ch>
2017-07-01 12:34:21 +0200

    implemented two programs and sendig signals

--- signalsExample/signal1.c
+++ signalsExample/signal1.c
@@ -1,13 +1,41 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <stdio.h>
+#include "string.h"
 #include <stdlib.h>
-#include <errno.h>
-#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
 
-int main()
-{    
+static int received = 0;
+
+void readSignal(int sig) {
+    if (sig == SIGUSR1) {
+        received = 1;
+    }
+}
+
+int main(int argc, char * argv[]) {
+    signal(SIGUSR1, readSignal);
+    
+    int pid = fork();
+    switch (pid) {
+        case -1:
+            printf("Fork Failed\n");
+            break;
+        case 0:
+            printf("Procesi femije pid = %d\n",getpid());
+            execl("./signal2","child",NULL);
+            break;
+        default:
+            wait((int *) 0);
+            printf("Procesi prind pid = %d\n", getpid());
+            printf("child sleep completed\n");
+            
+            while (!received);
+            printf("SIGURS1 received\n");
+            
+            exit(0);
+            break;
+    }
     return 0;
 }
--- signalsExample/signal2.c
+++ signalsExample/signal2.c
@@ -1,13 +1,25 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <stdio.h>
+#include "string.h"
 #include <stdlib.h>
-#include <errno.h>
-#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
 
-int main()
-{
+int main(int argc, char * argv[]) {
+
+    pid_t ppid;
+    ppid = getppid();
+    
+    printf("Before sleep\n");
+    printf("parent pid => %d\n",ppid);
+    
+    for (int i = 0; i <= 2; i++) {
+        printf("%d\n",i);
+        sleep(1);
+    }
+
+    kill(ppid, SIGUSR1);
+    
     return 0;
 }
