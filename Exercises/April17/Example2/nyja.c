c2ea3a5b582141fd9f992f5b1d4db8bf6f186d0b
Egzon Arifi <egzon.arifi@jaywalker-digital.ch>
2017-07-04 09:48:51 +0200

    changes in implementation of named pipes example

--- Exercises/April17/Example2/nyja.c
+++ Exercises/April17/Example2/nyja.c
@@ -1,10 +1,17 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <string.h>
-#include <getopt.h>
-#include <stdbool.h>
+
+#define MAX_BUF 1024
+#define PERM 0666
+
 
 int main(int argc, char * argv[]) {
-    
+
     return 0;
 }
--- Exercises/April17/Example2/rrjeta.c
+++ Exercises/April17/Example2/rrjeta.c
@@ -1,10 +1,29 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <string.h>
-#include <getopt.h>
-#include <stdbool.h>
+
+#define MAX_BUF 1024
 
 int main(int argc, char * argv[]) {
+    
+    int fd;
+    char *fifo = "myfifo";
+    char text[MAX_BUF];
+    
+    for (int i = 0; i < 5; i++) {
+        printf("%d",i);
+        sleep(1);
+    }
+    
+    fd = open(fifo, O_RDONLY);
+    read(fd, text, MAX_BUF);
+    printf("Received: %s\n", text);
+    close(fd);
     
     return 0;
 }
